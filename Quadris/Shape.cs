880290f1c056d84d5945275b503d3218b857bd2f
Berk A <sas41@users.noreply.github.com>
2018-04-18 18:15:51 +0300

    Initial commit

---
+++ Quadris/Shape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadris
+{
+    class Shape
+    {
+        public List<Tetromino> phases = new List<Tetromino>();
+        public int phaseCount;
+        public int phase = 0;
+        public int x = 4, y = 0;
+
+        public static Shape Copy(Shape source)
+        {
+            Shape copy = new Shape();
+            copy.phaseCount = source.phaseCount;
+            copy.phase = source.phase;
+            copy.x = source.x;
+            copy.y = source.y;
+
+            foreach (var item in source.phases)
+            {
+                copy.phases.Add(item);
+            }
+            return copy;
+        }
+    }
+}
---
+++ Quadris/Tetromino.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadris
+{
+    class Tetromino
+    {
+        public char[,] matrix;
+        public int padding;
+        public Tetromino(char[,] mat, int pad)
+        {
+            matrix = mat;
+            padding = pad;
+        }
+    }
+}
